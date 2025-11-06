7aa4ac78932011f9700378ee779c5a73c6b2b73a
umbra-mancer <darklyfalse@tutamail.com>
2025-11-06 06:47:26 +0530

    Adding DriverFactory

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,9 +1,13 @@
-import org.junit.Test;
+import driver.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
 public class LoginTests {
 
     @Test
     public void validLogin() {
-
+        WebDriver driver= DriverFactory.getDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.quit();
     }
 }
---
+++ src/test/java/driver/DriverFactory.java
@@ -0,0 +1,12 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    public static WebDriver getDriver(){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+}
